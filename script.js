edecd40fb964a80ff7fbd2d54d6382b9f7d46b69
kennybandy <nekbandy.com>
2023-07-30 23:41:51 +0100

    Add the task and Pseudocode

---
+++ script.js
@@ -0,0 +1,12 @@
+//THE TASK
+//To Create a Simple Calculator
+//The calculator takes 2 Inputs is from the User:
+//The User Inputs current year and birth year, the input the User put in is string and needs to be converted to number
+//To calculate the age
+//To return the age, how to return the age to the User?
+
+//PSEUDOCODE TO SOLVE THE TASK
+// 1. Take in 2 Input currentYear and birthYear:
+// 2. Conversion of the Inputs to string
+// 3. The function takes in the 2 inputs and calculate the age
+// 4. Returning the age to the User
