274508f0d321459f77a9727d28348807a16f1d3c
kennybandy <nekbandy.com>
2023-07-31 00:40:24 +0100

    Call function and return age

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-//THE TASK
-//To Create a Simple Calculator
-//The calculator takes 2 Inputs is from the User:
-//The User Inputs current year and birth year, the input the User put in is string and needs to be converted to number
-//To calculate the age
-//To return the age, how to return the age to the User?
-
 //PSEUDOCODE TO SOLVE THE TASK
 // Take in 2 Input currentYear and birthYear:
 // Conversion of the Inputs to string
@@ -12,7 +5,12 @@ const birthYear = parseInt(prompt("Enter your birth year"));
 let currentYear = parseInt(prompt("Enter the current year"));
 
 // The function takes in the 2 inputs and calculate the age
+function calculateAge(currentYear, birthYear) {
+  return currentYear - birthYear;
+}
 
 // Calling of the function
+const age = calculateAge(currentYear, birthYear);
 
 // Returning the age to the User
+prompt(age);
